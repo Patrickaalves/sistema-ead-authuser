20fd691e8380d4f2fa5fd09263722f5fdc189f3f
Patrickaalves <patrickalvessc@gmail.com>
2025-01-10 07:27:50 -0300

    Criando tratamento de excecoes

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -4,11 +4,10 @@ import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("users")
@@ -24,4 +23,18 @@ public class UserController {
     public ResponseEntity<List<UserModel>> getAllUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
     }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(userId).get());
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId) {
+        userService.delete(userService.findById(userId).get());
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
+    }
+
+
+
 }
