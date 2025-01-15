2759dbd75862bc11ac2b2c372b1a1700c848aef3
Patrickaalves <patrickalvessc@gmail.com>
2025-01-14 22:34:26 -0300

    Criando uma paginacao default a nivel de metodo

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -4,6 +4,10 @@ import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -24,8 +28,10 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<List<UserModel>> getAllUsers() {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    public ResponseEntity<Page<UserModel>> getAllUsers(@PageableDefault(page = 0, size = 3, sort = "userId") Pageable pageable) {
+
+        Page<UserModel> userModelPage = userService.findAll(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
     }
 
     @GetMapping("/{userId}")
--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -2,6 +2,8 @@ package com.ead.authuser.services;
 
 import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +11,7 @@ import java.util.UUID;
 
 public interface UserService {
     List<UserModel> findAll();
+    Page<UserModel> findAll(Pageable pageable);
     Optional<UserModel> findById(UUID userId);
     void delete(UserModel userModel);
     UserModel registerUser(UserRecordDto userRecordDto);
@@ -17,4 +20,5 @@ public interface UserService {
     UserModel updateUser(UserRecordDto userRecordDto, UserModel userModel);
     UserModel updatePassword(UserRecordDto userRecordDto, UserModel userModel);
     UserModel updateImage(UserRecordDto userRecordDto, UserModel userModel);
+
 }
