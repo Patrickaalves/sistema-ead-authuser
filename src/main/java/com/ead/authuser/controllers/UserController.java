a6a3e0646e458d6eca6fe662d155f1dfc55fc031
Patrickaalves <patrickalvessc@gmail.com>
2025-01-14 22:41:51 -0300

    Criando uma paginacao globlal

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -6,14 +6,11 @@ import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -28,8 +25,7 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<UserModel>> getAllUsers(@PageableDefault(page = 0, size = 3, sort = "userId") Pageable pageable) {
-
+    public ResponseEntity<Page<UserModel>> getAllUsers(Pageable pageable) {
         Page<UserModel> userModelPage = userService.findAll(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
     }
