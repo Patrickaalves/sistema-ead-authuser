413bb03ae3b9e4d29611a6c2e7e9d356b3b1831f
Patrickaalves <patrickalvessc@gmail.com>
2025-01-14 22:10:29 -0300

    Criando uma validacao customizada

---
+++ src/main/java/com/ead/authuser/validations/PasswordConstraint.java
@@ -0,0 +1,23 @@
+package com.ead.authuser.validations;
+
+import com.ead.authuser.validations.impl.PasswordConstraintImpl;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordConstraintImpl.class) // classe que vai implementar as regras que definimos
+@Target({ElementType.METHOD, ElementType.FIELD}) // Anotação a nivel de metodo e atributo
+@Retention(RetentionPolicy.RUNTIME) // Quando que a anotação ira ocorrer
+public @interface PasswordConstraint {
+    // Parametros padrão do bean validation
+    String message() default """ 
+            Invalid password! Password must contain at least one digit [0-9],
+            at leasty one lowercase Latin character [a-z], at least one uppercase
+            Latin character [A-Z], at least one special character like !@#&()-:;',?/*~$^+=<>,
+            A length od at least 6 characters and a maximun of 20 characters """;
+
+    Class<?> [] groups() default {}; // Um array de classes
+    Class<? extends Payload>[] payload() default {}; // Um array de classes que extendem a classe Payload
+}
---
+++ src/main/java/com/ead/authuser/validations/impl/PasswordConstraintImpl.java
@@ -0,0 +1,35 @@
+package com.ead.authuser.validations.impl;
+
+import com.ead.authuser.validations.PasswordConstraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class PasswordConstraintImpl implements ConstraintValidator<PasswordConstraint, String> { // Terei que validar nesse caso o tipo String
+
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{6,20}$";
+
+    @Override
+    public void initialize(PasswordConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        // password : Valor dentro do campo que recebo, se for enviado o valor na senha 77TEste,
+        // o campo password ira ter essa informação
+
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+
+        if (password == null || password.trim().isEmpty() || password.contains(" ")) {
+            return false;
+        } else if (!pattern.matcher(password).matches()){
+            return false;
+        }
+
+        return true;
+    }
+
+
+}
