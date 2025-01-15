63d3650cc389dd4daa714308b573d2c133971e58
Patrickaalves <patrickalvessc@gmail.com>
2025-01-14 22:58:36 -0300

    Adicionando politica de cors a aplicacao

--- src/main/java/com/ead/authuser/configs/ResolverConfig.java
+++ src/main/java/com/ead/authuser/configs/ResolverConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,7 +16,15 @@ public class ResolverConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         var pageableResolver = new PageableHandlerMethodArgumentResolver();
-        pageableResolver.setFallbackPageable(PageRequest.of(0, 2));
+        pageableResolver.setFallbackPageable(PageRequest.of(0, 5));
         argumentResolvers.add(pageableResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // adicionando para cada controller
+        // No caso do user controller ele tera todas origens permitidas
+        //registry.addMapping("/users/**").allowedOrigins("*");
+        registry.addMapping("/**").allowedOrigins("*"); // para todos endpoints terao origem permitida
     }
 }
