0f791a1f2d17a67d2414e794f337a7ffbeb6afa8
Patrickaalves <patrickalvessc@gmail.com>
2025-01-30 22:14:44 -0300

    Criando mapeamento de api composition para courses

---
+++ src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -0,0 +1,14 @@
+package com.ead.authuser.services.impl;
+
+import com.ead.authuser.repositories.UserCourseRepository;
+import com.ead.authuser.services.UserCourseService;
+
+public class UserCourseServiceImpl {
+    private final UserCourseRepository userCourseRepository;
+
+    public UserCourseServiceImpl(UserCourseRepository userCourseRepository) {
+        this.userCourseRepository = userCourseRepository;
+    }
+
+    
+}
